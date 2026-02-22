58baed3664e2303d4b582beb8bb4f9a6c1fd02c4
John Doe <john.doe@contoso.com>
2026-02-22 19:22:59 +0100

    Integración de PostgreSql con varaibles de entorno

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // index.js
 const http = require('http');
+const { Client } = require('pg');
 
-// Una función simple que queremos asegurar que funcione
+// Configuración de conexión (leerá variables de entorno)
+const dbClient = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+// Intentamos conectar al arrancar
+dbClient.connect()
+  .then(() => console.log("¡Conectado a la BD de AWS!"))
+  .catch(err => console.error("Error al conectar:", err));
+
 function sumar(a, b) {
   return a + b;
 }
@@ -9,12 +23,11 @@ function sumar(a, b) {
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Automatización Total Nivel 1 completa');
+  res.end('Automatización Total Nivel 2: BD Integrada');
 });
 
-// Exportamos la función para el test
 module.exports = { sumar, server };
 
 if (require.main === module) {
-  server.listen(3000, () => console.log('Puerto 3000'));
+  server.listen(3000, () => console.log('Servidor en puerto 3000'));
 }
